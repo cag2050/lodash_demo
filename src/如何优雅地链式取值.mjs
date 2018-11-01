e578883889a98b14f79517593766bda0f9238221
chen_anguo <cag2050@163.com>
2018-11-01 20:48:09 +0800

    m

---
+++ src/如何优雅地链式取值.mjs
@@ -0,0 +1,8 @@
+如何优雅地链式取值：https://juejin.im/post/5ba08483e51d450e99430a7f
+
+const _ = require('lodash')
+
+var object = {a: [{b: {c: 3}}]};
+var result = _.get(object, 'a[0].b.c', 1);
+console.log(result);
+// output: 3
